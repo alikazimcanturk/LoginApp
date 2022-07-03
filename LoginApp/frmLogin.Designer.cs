c6711ea07059aebbaf6881426e22adf0418ee243
Ali Kazım Cantürk <alikazimcanturk@gmail.com>
2022-07-04 02:50:59 +0300

    Add project files.

---
+++ LoginApp/frmLogin.Designer.cs
@@ -0,0 +1,175 @@
+﻿
+namespace LoginApp
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtUserName = new MetroFramework.Controls.MetroTextBox();
+            this.txtPassword = new MetroFramework.Controls.MetroTextBox();
+            this.chkRememberMe = new MetroFramework.Controls.MetroCheckBox();
+            this.btnLogin = new MetroFramework.Controls.MetroButton();
+            this.metroStyleManager1 = new MetroFramework.Components.MetroStyleManager(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::LoginApp.Properties.Resources.indir;
+            this.pictureBox1.Location = new System.Drawing.Point(40, 78);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(144, 141);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtUserName
+            // 
+            // 
+            // 
+            // 
+            this.txtUserName.CustomButton.Image = null;
+            this.txtUserName.CustomButton.Location = new System.Drawing.Point(192, 1);
+            this.txtUserName.CustomButton.Name = "";
+            this.txtUserName.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.txtUserName.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.txtUserName.CustomButton.TabIndex = 1;
+            this.txtUserName.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.txtUserName.CustomButton.UseSelectable = true;
+            this.txtUserName.CustomButton.Visible = false;
+            this.txtUserName.DisplayIcon = true;
+            this.txtUserName.Icon = global::LoginApp.Properties.Resources.icons8_male_user_16;
+            this.txtUserName.Lines = new string[0];
+            this.txtUserName.Location = new System.Drawing.Point(216, 95);
+            this.txtUserName.MaxLength = 32767;
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.PasswordChar = '\0';
+            this.txtUserName.PromptText = "Lütfen kullanıcı adınızı giriniz";
+            this.txtUserName.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txtUserName.SelectedText = "";
+            this.txtUserName.SelectionLength = 0;
+            this.txtUserName.SelectionStart = 0;
+            this.txtUserName.ShortcutsEnabled = true;
+            this.txtUserName.Size = new System.Drawing.Size(214, 23);
+            this.txtUserName.TabIndex = 0;
+            this.txtUserName.UseSelectable = true;
+            this.txtUserName.WaterMark = "Lütfen kullanıcı adınızı giriniz";
+            this.txtUserName.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.txtUserName.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // txtPassword
+            // 
+            // 
+            // 
+            // 
+            this.txtPassword.CustomButton.Image = null;
+            this.txtPassword.CustomButton.Location = new System.Drawing.Point(192, 1);
+            this.txtPassword.CustomButton.Name = "";
+            this.txtPassword.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.txtPassword.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.txtPassword.CustomButton.TabIndex = 1;
+            this.txtPassword.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.txtPassword.CustomButton.UseSelectable = true;
+            this.txtPassword.CustomButton.Visible = false;
+            this.txtPassword.DisplayIcon = true;
+            this.txtPassword.Icon = global::LoginApp.Properties.Resources.icons8_password_key_16;
+            this.txtPassword.Lines = new string[0];
+            this.txtPassword.Location = new System.Drawing.Point(216, 124);
+            this.txtPassword.MaxLength = 32767;
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '\0';
+            this.txtPassword.PromptText = "Lütfen şifrenizi giriniz.";
+            this.txtPassword.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txtPassword.SelectedText = "";
+            this.txtPassword.SelectionLength = 0;
+            this.txtPassword.SelectionStart = 0;
+            this.txtPassword.ShortcutsEnabled = true;
+            this.txtPassword.Size = new System.Drawing.Size(214, 23);
+            this.txtPassword.TabIndex = 1;
+            this.txtPassword.UseSelectable = true;
+            this.txtPassword.WaterMark = "Lütfen şifrenizi giriniz.";
+            this.txtPassword.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.txtPassword.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // chkRememberMe
+            // 
+            this.chkRememberMe.AutoSize = true;
+            this.chkRememberMe.Location = new System.Drawing.Point(216, 153);
+            this.chkRememberMe.Name = "chkRememberMe";
+            this.chkRememberMe.Size = new System.Drawing.Size(101, 15);
+            this.chkRememberMe.TabIndex = 2;
+            this.chkRememberMe.Text = "Remember Me";
+            this.chkRememberMe.UseSelectable = true;
+            this.chkRememberMe.CheckedChanged += new System.EventHandler(this.chkRememberMe_CheckedChanged);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(355, 196);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 3;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseSelectable = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // metroStyleManager1
+            // 
+            this.metroStyleManager1.Owner = null;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(655, 304);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.chkRememberMe);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "frmLogin";
+            this.Text = "Login Page";
+            this.Load += new System.EventHandler(this.frmLogin_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private MetroFramework.Controls.MetroTextBox txtUserName;
+        private MetroFramework.Controls.MetroTextBox txtPassword;
+        private MetroFramework.Controls.MetroCheckBox chkRememberMe;
+        private MetroFramework.Controls.MetroButton btnLogin;
+        private MetroFramework.Components.MetroStyleManager metroStyleManager1;
+    }
+}
---
+++ LoginApp/frmLogin.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Dapper;
+
+namespace LoginApp
+{
+    public partial class frmLogin : MetroFramework.Forms.MetroForm
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+            this.StyleManager = metroStyleManager1;
+            metroStyleManager1.Theme = MetroFramework.MetroThemeStyle.Light;
+            metroStyleManager1.Style = MetroFramework.MetroColorStyle.Blue;
+
+            if (Properties.Settings.Default.RememberMe)
+            {
+                txtUserName.Text = Properties.Settings.Default.UserName;
+                txtPassword.Text = Properties.Settings.Default.Password;
+
+            }
+        }
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtUserName.Text))
+            {
+                MetroFramework.MetroMessageBox.Show(this, "Lütfen kullanıcıadınızı giriniz.", "Message", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtUserName.Focus();
+                return;
+            }
+            try
+            {
+                using (IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["cn"].ConnectionString))
+                {
+                    if (db.State == ConnectionState.Closed)
+                        db.Open();
+                    User obj = db.Query<User>($"select *from Users where UserName = '{txtUserName.Text}'", commandType: CommandType.Text).SingleOrDefault();
+                    if (obj != null)
+                    {
+                        if (obj.Password == txtPassword.Text)
+                        {
+                            using (frmMain frm = new frmMain())
+                            {
+                                this.Hide();
+                                frm.ShowDialog();
+                            }
+                        }
+                        else
+                            MetroFramework.MetroMessageBox.Show(this, "Lütfen kullanıcıadınızı giriniz.", "Message", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                    else
+                        MetroFramework.MetroMessageBox.Show(this, "Kullanıcı adı veya şifre hatalı.", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MetroFramework.MetroMessageBox.Show(this, ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void chkRememberMe_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkRememberMe.Checked)
+            {
+                Properties.Settings.Default.UserName = txtUserName.Text;
+                Properties.Settings.Default.Password = txtPassword.Text;
+            }
+            else
+            {
+                Properties.Settings.Default.UserName = null;
+                Properties.Settings.Default.Password = null;
+            }
+            Properties.Settings.Default.RememberMe = chkRememberMe.Checked;
+            Properties.Settings.Default.Save();
+        }
+
+        
+    }
+    
+}
